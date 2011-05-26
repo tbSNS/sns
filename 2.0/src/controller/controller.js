a3951f8e1c3237a53859db00a2c6c6ce38630297
zhangquan <zhang.quan.app@gmail.com>
2011-05-26 16:51:51 +0800

    修改目录

---
+++ 2.0/src/controller/controller.js
@@ -0,0 +1,12 @@
+module.declare(function(require,exports,module){
+    var controller =function(){
+        
+    }
+    controller.controll =function(cfg){
+        this.moduleContext.load("")
+    }
+    controller.moduleContext =true;
+
+    return controller;
+
+})
